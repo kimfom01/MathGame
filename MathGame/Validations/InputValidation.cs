b8fbc71ba4a7f1964bbe0dafc0e6e9f15a937fcf
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-04-26 12:57:17 +0300

    Make Validation methods accept nullable strings

--- MathGame/Validations/InputValidation.cs
+++ MathGame/Validations/InputValidation.cs
@@ -2,12 +2,12 @@
 
 public class InputValidation
 {
-    public bool ValidateInput(string choice)
+    public bool ValidateInput(string? choice)
     {
         return !string.IsNullOrWhiteSpace(choice);
     }
 
-    public bool ValidateChoice(string input, out int choice)
+    public bool ValidateChoice(string? input, out int choice)
     {
         choice = -1;
         return !string.IsNullOrWhiteSpace(input) && int.TryParse(input, out choice);
