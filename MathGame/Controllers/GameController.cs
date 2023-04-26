dd1b3505cc04411736323609652953b6c41ab41d
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-04-26 13:32:29 +0300

    Add a function that let's the user pick the number of questions

--- MathGame/Controllers/GameController.cs
+++ MathGame/Controllers/GameController.cs
@@ -78,12 +78,29 @@ public class GameController
 
     private void PlayGameRound(Operation operation)
     {
+        Console.WriteLine("How many questions do you want?");
+        int count = GetNumberOfQuestions();
+        
+        var score = 0;
+
         var timer = new Stopwatch();
         timer.Start();
 
-        int count = 0, score = 0;
+        score = GameLoop(operation, count, score);
 
-        for (int i = 0; i < 10; i++)
+        timer.Stop();
+
+        int time = (int)timer.Elapsed.TotalSeconds;
+
+        Console.WriteLine($"You scored {score} out of {count} in {time} seconds");
+        _gameHistory.Add($"Game: {operation} | Score: {score} out of {count} | Duration: {time} seconds");
+        Console.WriteLine("Press Enter to continue: ");
+        Console.ReadLine();
+    }
+
+    private int GameLoop(Operation operation, int count, int score)
+    {
+        for (int i = 0; i < count; i++)
         {
             var numA = Random.Shared.Next(1, 10);
             var numB = Random.Shared.Next(1, 10);
@@ -108,19 +125,12 @@ public class GameController
                 Console.WriteLine("Wrong!");
                 Console.WriteLine($"The answer was {answer}");
             }
-
-            count++;
         }
 
-        timer.Stop();
-
-        int time = (int)timer.Elapsed.TotalSeconds;
-
-        Console.WriteLine($"You scored {score} out of {count} in {time} seconds");
-        _gameHistory.Add($"Game - {operation} - Score: {score} out of {count} Duration - {time} seconds");
-        Console.WriteLine("Press Enter to continue: ");
-        Console.ReadLine();
+        return score;
     }
+
+    private int GetNumberOfQuestions() => _input.GetChoice();
 
     private string CreateQuestion(int numA, int numB, Operation operation)
     {
